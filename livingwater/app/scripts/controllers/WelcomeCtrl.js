55d0b2d7716465e121b70534316545e8cd39836d
Mark Wilkinson <markawil@yahoo.com>
2016-10-22 13:00:52 -0500

    showing the menu list of items

--- livingwater/app/scripts/controllers/WelcomeCtrl.js
+++ livingwater/app/scripts/controllers/WelcomeCtrl.js
@@ -2,6 +2,20 @@
 
 var WelcomeCtrl = function($scope) {
   $scope.testVar = 'We running from a required module! ';
+
+  $scope.menuItems = ["Thirst Gauge", "Why Water?", "Why Us?", "Take Action", "About Living Water", "Store"];
 };
 
 module.exports = WelcomeCtrl;
+
+var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  // Toggle button
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
